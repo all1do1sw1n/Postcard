79465153694937ecaee145b7670a483aebb75e7f
all1do1sw1n <matthew@partymashup.net>
2014-11-03 20:01:28 -0600

    save meow

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -32,7 +32,6 @@ class ViewController: UIViewController {
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the send mail button
-        // Test comment 101 - 102
         
         // Hidden label to display message text
         messageLabel.hidden = false
